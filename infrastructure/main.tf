952c3af3f7bc54bad45ff74e554a453cfd1bf7bf
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-06 16:55:11 -0400

    test bad format

---
+++ infrastructure/main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-terraform"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "demostorageacct${random_integer.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
