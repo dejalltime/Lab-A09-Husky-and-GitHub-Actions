c6add1e7f7598719f56435b008faab99670655f8
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-06 19:12:18 -0400

    error fixed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     "demostorageacct${random_integer.rand.result}"
-  resource_group_name      azurerm_resource_group.rg.name
+  name                     = "demostorageacct${random_integer.rand.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
