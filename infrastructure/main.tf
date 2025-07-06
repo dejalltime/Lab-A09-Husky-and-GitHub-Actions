28d0dc1711d26653a6a16c2a610c15eec1111788
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-06 16:55:24 -0400

    test bad format

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = "demostorageacct${random_integer.rand.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
+    account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
